a0867a9c2ce989821ec45324e7963518371404a4
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-07-21 15:41:41 +0200

    'handelRegisterErrorMessage'

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -46,7 +46,7 @@ async function senduser(info){
 const [errors,seterrors]=useState([])
 function validation(){
   let rules=Joi.object({
-    "first_name": Joi.string().alphanum().min(3).max(30).required(),
+    "first_name": Joi.string().min(3).max(30).required(),
     "last_name":Joi.string().alphanum().min(3).max(30).required(),
     "email":Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     "password":Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
@@ -57,45 +57,60 @@ let validationresult=  rules.validate(userdata,{abortEarly:false})
 console.log(validationresult.error.details);
 if (validationresult.error !==undefined) {
   seterrors(validationresult.error.details)
-  return true;
-}else{return false}}
+  return false;
+}else{return true}}
   return (
     <>
     <div className='container mt-3'>
       <h1 className='text-center text-capitalize mb-3'>Register Form</h1>
-      {errors.length!==0? errors.map((error)=>{return <p className='text-danger'>{error.message}</p>}):""}
-      <form className='w-50 m-auto p-4 ' action="" onSubmit={(EI)=>{
+      {/* {errors.length!==0? errors.map((error)=>{ return <p className='text-danger'>{error.message}</p>}):""} */}
+      {/* {errors.length!==0? errors.filter((error)=>error.contain('first_name')):""} */}
+      <form className='w-50 m-auto p-3 ' action="" onSubmit={(EI)=>{
         senduser(EI)
         
         }}>
         
-        <label htmlFor="fname" className='mb-2'>First Name :</label>
-        <input id='fname' type="text" className='form-control mb-2' name='first_name' onChange={(e)=>{
+        <label htmlFor="fname" className='mb-2 mt-2'>First Name :</label>
+        <input id='fname' type="text" className='form-control ' name='first_name' onChange={(e)=>{
           adduser(e.target)
         }} />
+        {errors.length!==0?errors.map((error)=> error.message.includes('first_name')? <h6 className='text-danger'>{
+        // error.message
+        // use ternary operator to cusotme error message
+        error.message.includes("alpha-numeric")?error.message="please must contain character only":error.message
+        }</h6>:""):""}
+
         
-        <label htmlFor="lname" className='mb-2'>Last Name :</label>
-        <input id='lname' type="text" className='form-control mb-2' name='last_name' onChange={(e)=>{
+        <label htmlFor="lname" className='mb-2 mt-2'>Last Name :</label>
+        <input id='lname' type="text" className='form-control ' name='last_name' onChange={(e)=>{
           
           adduser(e.target)
         }} />
+        {errors.length!==0?errors.map((error)=> error.message.includes('last_name')? <h6 className='text-danger'>{error.message}</h6>:""):""}
+
         
-        <label htmlFor="age" className='mb-2'>Age :</label>
-        <input id='age' type="text" className='form-control mb-2' name='age' onChange={(e)=>{
+        <label htmlFor="age" className='mb-2 mt-2'>Age :</label>
+        <input id='age' type="text" className='form-control ' name='age' onChange={(e)=>{
           adduser(e.target)
         }} />
-        
-        <label htmlFor="email" className='mb-2'>Email :</label>
-        <input id='email' type="text" className='form-control mb-2' name='email' onChange={(e)=>{
+        {errors.length!==0?errors.map((error)=> error.message.includes('age')? <h6 className='text-danger'>{error.message}</h6>:""):""}
+
+       
+        <label htmlFor="email" className='mb-2 mt-2'>Email :</label>
+        <input id='email' type="text" className='form-control ' name='email' onChange={(e)=>{
           adduser(e.target)
         }}/>
+        {errors.length!==0?errors.map((error)=> error.message.includes('email')? <h6 className='text-danger'>{error.message}</h6>:""):""}
+
         
-        <label htmlFor="password" className='mb-2'>Password :</label>
-        <input id='password' type="text" className='form-control mb-4' name='password' onChange={(e)=>{
+        <label htmlFor="password" className='mb-2 mt-2'>Password :</label>
+        <input id='password' type="password" className='form-control ' name='password' onChange={(e)=>{
           adduser(e.target)
         }} />
+       {errors.length!==0?errors.map((error)=> error.message.includes('password')? <h6 className='text-danger'>{"password should have numbers-letters"}</h6>:""):""}
+
         
-        <button type='onSubmit' className='btn btn-info'>Register</button>
+        <button type='onSubmit' className='btn btn-info mt-2'>Register</button>
         <h3 className='text-success'>{msg}</h3>
       </form>
       
