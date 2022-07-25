446c1807e3dcb8214e73021685c6b3768bfedc43
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-07-25 10:49:17 +0200

    'home-fetchMovieDB-dispalyData'

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,141 @@
-import React from 'react'
+// import React from 'react'
+import {React, useState ,useEffect} from 'react'
+import axios from 'axios'
+import Movies from './../Movies/Movies';
+
 
 export default function Home() {
+  
+  const [movies, setmovies]=useState([])
+  const [tvs, settvs]=useState([])
+  const [persons, settperson]=useState([])
+  
+ async function getdata(mediatype,setterfunction){
+ let {data}= await axios.get('https://api.themoviedb.org/3/trending/'+mediatype+'/day?api_key=a5208e5b49c6070ccb08e9c12afd5949')
+ setterfunction(data.results);
+ console.log(movies);
+}
+// async function gettvs(){
+//   let {data}= await axios.get('https://api.themoviedb.org/3/trending/tv/day?api_key=a5208e5b49c6070ccb08e9c12afd5949')
+//   settvs(data.results);
+//   console.log(movies); 
+//  }
+useEffect(()=>{
+  
+  getdata('movie',setmovies)
+  getdata('tv',settvs)
+  getdata('person',settperson)
+ 
+ })
+ 
   return (
-    <div>Home</div>
+    <>
+       <div className='container pt-3 '>
+        <div className='row'>
+          <div className='col-md-4 d-flex align-items-center  '>
+            
+          <div>
+          <div className='border border-primary w-25 mb-4'></div>
+             <h1>trinding<br/>Movies <br/> watch now</h1>
+             <p>most watches movies by day</p>
+          <div className='border border-primary w-75 mt-4 '></div>
+          </div>
+          </div>
+          {/* 7al tany 3alchan a3rd 9 movies */}
+          {/* {movies.slice(0 , 10).map((movie) => {
+            return(
+              <div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className='w-100' alt="" />
+                <h4>{movie.title}</h4>
+              </div>
+              </div>
+            )
+          }) */}
+           {movies.map((movie,index) => {
+            return(
+              index <= 9?<div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className='w-100' alt="" />
+                <h4>{movie.title}</h4>
+              </div>
+              </div>:""
+            )
+          })
+            }
+        </div>
+        <div className='row'>
+          <div className='col-md-4 d-flex align-items-center  '>
+            
+          <div>
+          <div className='border border-primary w-25 mb-4'></div>
+             <h1>trinding<br/>tv <br/> watch now</h1>
+             <p>most watches movies by day</p>
+          <div className='border border-primary w-75 mt-4 '></div>
+          </div>
+          </div>
+          {/* 7al tany 3alchan a3rd 9 movies */}
+          {/* {movies.slice(0 , 10).map((movie) => {
+            return(
+              <div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className='w-100' alt="" />
+                <h4>{movie.title}</h4>
+              </div>
+              </div>
+            )
+          }) */}
+           {tvs.map((tv,index) => {
+            return(
+              index <= 9?<div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${tv.poster_path}`} className='w-100' alt="" />
+                <h4>{tv.title}</h4>
+              </div>
+              </div>:""
+            )
+          })
+            }
+        </div>
+      
+      <div className='row'>
+          <div className='col-md-4 d-flex align-items-center  '>
+            
+          <div>
+          <div className='border border-primary w-25 mb-4'></div>
+             <h1>trinding<br/>person <br/> watch now</h1>
+             <p>most watches movies by day</p>
+          <div className='border border-primary w-75 mt-4 '></div>
+          </div>
+          </div>
+          {/* 7al tany 3alchan a3rd 9 movies */}
+          {persons.slice(0 , 10).map((person) => {
+            return(
+              <div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} className='w-100' alt="not found" />
+                <h4>{person.title}</h4>
+              </div>
+              </div>
+            )
+          })}
+           {/* {persons.map((person,index) => {
+            return(
+              index <= 9?<div className='col-md-2'>
+              <div>
+                <img src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} className='w-100' alt="" />
+                <h4>{person.name}</h4>
+              </div>
+              </div>:""
+            )
+          })
+            } */}
+        </div>
+      </div>
+      
+      
+     
+    
+    </>
   )
 }
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,109 @@
-import React from 'react'
+import {React,useState} from 'react'
+import axios from 'axios'
+// import { Joi } from 'joi';   will make error  wrong import
+import  Joi  from 'joi';     //  right import
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+  
+//#region  add  set user function updater >>>   adduser(x)
+
+  const[userdata,setuser]=useState({
+    "email":"",
+    "password":"",
+  })
+
+function adduser(x){
+  let useratacopy={...userdata}     //make deep copy
+  useratacopy[x.name]=  x.value        // make change in deep copy
+  setuser (useratacopy)  // pass deep copy to updater function 
 }
+ //#endregion  
+
+//#region     validation
+// state
+const [errors,seterrors]=useState([])
+function validation(){
+   let rules=Joi.object({
+     "email"     :Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+     "password"  :Joi.string(),
+   })
+   // let validationresult=  rules.validate(userdata,{abortEarly:true}) 
+   let validationresult=  rules.validate(userdata,{abortEarly:false})
+   console.log('validation',validationresult.error);
+   if (validationresult.error !==undefined) {
+   // console.log('validation',validationresult.error.details);
+   seterrors(validationresult.error.details)
+   return false;
+   }else{return true}
+}
+//#endregion
+
+//#region  ---------------------------------------------------------- api     senduser(info)  
+
+// useEffect(()=>{
+//   // console.table(userdata);  
+// }  
+// ,[userdata])
+
+ let navigation = useNavigate( )
+const [msg,setmsg]=useState('')
+const [btnspin,setbtnspin]=useState(true)
+
+async function senduser(info){
+ info.preventDefault()
+ setbtnspin(false)
+//  if(validation()=== true){   long syntax
+ if(validation()){  // short syntax
+  
+    // let response = await axios.post('https://route-egypt-api.herokuapp.com/signup',userdata)
+    let {data}= await axios.post('https://route-egypt-api.herokuapp.com/signin',userdata)
+    setbtnspin(true)
+    console.log('ibrahim',data.message); 
+    // setmsg(data.message)
+    if (data.message === 'success') {
+      navigation('/Home')
+    }
+    else
+    {
+      setmsg(data.message)
+    }
+  }
+  
+}
+
+//#endregion
+
+//#region     component function Register()  return
+
+  
+  return (
+    <>
+    <div className='container mt-3'>
+      <h1 className='text-center text-capitalize mb-3'>Login Form</h1>
+      {/* {errors.length!==0? errors.map((error)=>{ return <p className='text-danger'>{error.message}</p>}):""} */}
+      {/* {errors.length!==0? errors.filter((error)=>error.contain('first_name')):""} */}
+      
+      {/* <form className='w-50 m-auto p-3 ' action="" onSubmit={(EI)=>{senduser(EI)}}>     full syntax */}
+      <form className='w-50 m-auto p-3 ' action=""  onSubmit={EI=>senduser(EI)}> 
+             
+        <label htmlFor="email" className='mb-2 mt-2'>Email :</label>
+        <input id='email' type="text" className='form-control ' name='email' onChange={e=>adduser(e.target)}/>
+        {errors.length!==0?errors.map((error)=> error.message.includes('email')? <h6 className='text-danger'>{error.message}</h6>:""):""}
+
+        
+        <label htmlFor="password" className='mb-2 mt-2'>Password :</label>
+        <input id='password' type="text" className='form-control ' name='password' onChange={e=>adduser(e.target)} />
+        {/* {errors.length!==0?errors.map((error)=> error.message.includes('password')? <h6 className='text-danger'>{error.message}</h6>:""):""} */}
+        {errors.length!==0?errors.map((error)=><h6 className='text-danger'>{error.message}</h6>):""}
+
+        
+        <button type='onSubmit' className='btn btn-info mt-2'>{ btnspin? 'Login': <i className='fas fa-spin fa-spinner fa-1x'></i>}</button>
+        <h3 className='text-success'>{msg}</h3>
+      </form>
+      
+    </div>
+    </>
+  )
+ }
+
